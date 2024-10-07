d416985cc1451757bc2ea8db81449f157d2fa90a
Louis Dutton <louis.dutton@travelchapter.com>
2024-10-07 16:16:06 +0100

    basic player movement

--- src/screen_gameplay.c
+++ src/screen_gameplay.c
@@ -1,40 +1,62 @@
 #include "raylib.h"
+#include "raymath.h"
 #include "screens.h"
+#include <stdio.h>
 
-static int framesCounter = 0;
 static int finishScreen = 0;
+static bool isPaused = false;
+static Color bg_colour = PURPLE;
+
+static int x = 0;
+static int y = 0;
+static int width = 10;
+static int height = 10;
+
+static const int MOVE_SPEED = 5;
 
 // Gameplay Screen Initialization logic
 void InitGameplayScreen(void) {
-  // TODO: Initialize GAMEPLAY screen variables here!
-  framesCounter = 0;
   finishScreen = 0;
+  x = GetScreenWidth() / 2;
+  y = GetScreenHeight() / 2;
 }
 
 // Gameplay Screen Update logic
 void UpdateGameplayScreen(void) {
-  // TODO: Update GAMEPLAY screen variables here!
+  if (IsKeyPressed(KEY_P))
+    isPaused = !isPaused;
 
-  // Press enter or tap to change to ENDING screen
-  if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
-    finishScreen = 1;
-    PlaySound(fxCoin);
+  if (!isPaused) {
+    if (IsKeyDown(KEY_A))
+      x -= MOVE_SPEED;
+    if (IsKeyDown(KEY_D))
+      x += MOVE_SPEED;
+    if (IsKeyDown(KEY_W))
+      y -= MOVE_SPEED;
+    if (IsKeyDown(KEY_S))
+      y += MOVE_SPEED;
+
+    // Press enter or tap to change to ENDING screen
+    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
+      finishScreen = 1;
+      PlaySound(fxCoin);
+    }
   }
 }
 
 // Gameplay Screen Draw logic
 void DrawGameplayScreen(void) {
-  // TODO: Draw GAMEPLAY screen here!
-  DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
+  DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLACK);
   Vector2 pos = {20, 10};
-  DrawTextEx(font, "GAMEPLAY SCREEN", pos, font.baseSize * 3.0f, 4, MAROON);
-  DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+  DrawTextEx(font, "GAMEPLAY SCREEN", pos, font.baseSize * 3.0f, 4, WHITE);
+  DrawRectangle(x, y, width, height, WHITE);
+  if (isPaused) {
+    DrawText("PAUSED", 130, 220, 20, WHITE);
+  }
 }
 
 // Gameplay Screen Unload logic
-void UnloadGameplayScreen(void) {
-  // TODO: Unload GAMEPLAY screen variables here!
-}
+void UnloadGameplayScreen(void) {}
 
 // Gameplay Screen should finish?
 int FinishGameplayScreen(void) { return finishScreen; }
