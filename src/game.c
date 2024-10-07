ae89cde053c5e7f2c0067329fe1c3fbb5f8f80ae
Louis Dutton <louis.dutton@travelchapter.com>
2024-10-07 15:43:05 +0100

    defualt to gameplay screen

--- src/game.c
+++ src/game.c
@@ -9,7 +9,7 @@
 // Shared Variables Definition (global)
 // NOTE: Those variables are shared between modules through screens.h
 //----------------------------------------------------------------------------------
-GameScreen currentScreen = LOGO;
+GameScreen currentScreen = GAMEPLAY;
 Font font = {0};
 Music music = {0};
 Sound fxCoin = {0};
@@ -59,8 +59,8 @@ int main(void) {
   PlayMusicStream(music);
 
   // Setup and init first screen
-  currentScreen = LOGO;
-  InitLogoScreen();
+  currentScreen = GAMEPLAY;
+  /*InitLogoScreen();*/
 
 #if defined(PLATFORM_WEB)
   emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
@@ -331,7 +331,7 @@ static void UpdateDrawFrame(void) {
   if (onTransition)
     DrawTransition();
 
-  // DrawFPS(10, 10);
+  DrawFPS(10, 10);
 
   EndDrawing();
   //----------------------------------------------------------------------------------
