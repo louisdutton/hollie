8635646ca3dd4552a99a7b97983c049ccf87e883
Louis Dutton <louis.dutton@travelchapter.com>
2024-10-08 11:11:18 +0100

    add sprite animation

--- src/screen_gameplay.c
+++ src/screen_gameplay.c
@@ -1,41 +1,82 @@
 #include "raylib.h"
 
-static const Color BG_COLOUR = PURPLE;
-static const int MOVE_SPEED = 2;
+// anim
+#define TARGET_FPS 60
+#define FPS 24
+#define INTERVAL TARGET_FPS / FPS
+#define FRAME_WIDTH 96
+#define FRAME_HEIGHT 64
+#define ANIM_COUNT 3
 
-static bool isPaused = false;
-static int x = 0;
-static int y = 0;
+// player
+#define MOVE_SPEED 2
+
+typedef enum Animations {
+  IDLE,
+  RUN,
+  JUMP,
+} Animations;
+
+// player
 static int width = 20;
 static int height = 20;
-
-static Texture2D player_sprite = {0};
+static Vector2 position = {0, 0};
+static Vector2 velocity = {0, 0};
 static Color player_colour = WHITE;
-static float player_rotation;
-static Rectangle frame = {0, 0, 16, 16};
-static Rectangle destination = {0, 0, 16, 16};
-static Vector2 origin = {0, 0};
+static Rectangle rect = {0, 0, FRAME_WIDTH, FRAME_HEIGHT};
+
+static Texture2D animations[ANIM_COUNT] = {0};
+static const int frame_counts[ANIM_COUNT] = {9, 8, 9};
+static int frame_counter = 0;
+static int frame = 0;
+static int current_anim = 0;
+
+static bool is_paused = false;
 
 // Gameplay Screen Initialization logic
 void InitGameplayScreen(void) {
-  player_sprite =
+  position.x = (float)GetScreenWidth() / 2;
+  position.y = (float)GetScreenHeight() / 2;
+  animations[0] =
       LoadTexture("resources/characters/human/idle/base_idle_strip9.png");
+  animations[1] =
+      LoadTexture("resources/characters/human/run/base_run_strip8.png");
+  animations[2] =
+      LoadTexture("resources/characters/human/jump/base_jump_strip9.png");
 }
 
 // Gameplay Screen Update logic
 void UpdateGameplayScreen(void) {
+  // input
   if (IsKeyPressed(KEY_P))
-    isPaused = !isPaused;
+    is_paused = !is_paused;
 
-  if (!isPaused) {
-    if (IsKeyDown(KEY_A))
-      x -= MOVE_SPEED;
-    if (IsKeyDown(KEY_D))
-      x += MOVE_SPEED;
-    if (IsKeyDown(KEY_W))
-      y -= MOVE_SPEED;
-    if (IsKeyDown(KEY_S))
-      y += MOVE_SPEED;
+  // calculate velocity
+  if (!is_paused) {
+    velocity.x = (IsKeyDown(KEY_D) - IsKeyDown(KEY_A)) * MOVE_SPEED;
+    velocity.y = (IsKeyDown(KEY_S) - IsKeyDown(KEY_W)) * MOVE_SPEED;
+  }
+
+  // apply velocity
+  position.x += velocity.x;
+  position.y += velocity.y;
+
+  // choose anim
+  if (velocity.x != 0 || velocity.y != 0) {
+    current_anim = 1;
+  } else {
+    current_anim = 0;
+  }
+
+  // anim
+  frame_counter++;
+
+  if (frame_counter > INTERVAL) {
+    frame_counter = 0;
+    frame++;
+    if (frame > frame_counts[current_anim])
+      frame = 0;
+    rect.x = (float)frame * (float)FRAME_WIDTH;
   }
 }
 
@@ -44,17 +85,24 @@ void DrawGameplayScreen(void) {
   DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLUE);
 
   // player
-  DrawRectangleLines(x, y, width, height, WHITE);
-  DrawTexturePro(player_sprite, frame, destination, origin, player_rotation,
-                 player_colour);
+  DrawRectangleLines(position.x - (float)width / 2,
+                     position.y - (float)height / 2, width, height, WHITE);
+  Vector2 tex_pos = position;
+  tex_pos.x -= (float)FRAME_WIDTH / 2;
+  tex_pos.y -= (float)FRAME_HEIGHT / 2;
+  DrawTextureRec(animations[current_anim], rect, tex_pos, player_colour);
 
-  if (isPaused) {
+  if (is_paused) {
     DrawText("PAUSED", 130, 220, 20, WHITE);
   }
 }
 
 // Gameplay Screen Unload logic
-void UnloadGameplayScreen(void) { UnloadTexture(player_sprite); }
+void UnloadGameplayScreen(void) {
+  for (int i = 0; i < ANIM_COUNT; i++) {
+    UnloadTexture(animations[i]);
+  }
+}
 
 // Gameplay Screen should finish?
-int FinishGameplayScreen(void) { return false; }
+int FinishGameplayScreen(void) { return 0; }
